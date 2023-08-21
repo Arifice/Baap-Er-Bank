9c8fab85ab9468220f1087905a1cfef41b871cab
Arifice <arifice13@gmail.com>
2023-08-21 16:22:14 +0600

    added

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,36 @@
-document.getElementById('btn-deposit').addEventListener('click',function(){
-    const depositField=document.getElementById('btn-deposit');
-    const depositAmount=depositField.value;
-    console.log(depositAmount);
-})
+document.getElementById('btn-deposit').addEventListener('click',function(event){
+    const depositField=document.getElementById('deposit-field');
+    const newDepositAmount=depositField.value;    
+    const depositTotalElement=document.getElementById('deposit-total');
+    const previousDepositTotal=depositTotalElement.innerText;    
+    
+    depositField.value='';
+    const currentDeposit=parseFloat(previousDepositTotal)+parseFloat(newDepositAmount);
+    depositTotalElement.innerText=currentDeposit;
+
+    const balanceElement=document.getElementById('balance-total');
+    const priviousBalance=balanceElement.innerText;
+    const currentBalance=parseFloat(priviousBalance)+currentDeposit;
+    balanceElement.innerText=currentBalance;
+
+});
+
+document.getElementById('btn-withdraw').addEventListener('click',function(event){
+    const withDrawField=document.getElementById('withdraw-field');
+    const newWithDrawAmount=withDrawField.value;
+    
+    const withDrawTotalElement=document.getElementById('withdraw-total');
+    const previousWithdrawTotal=withDrawTotalElement.innerText;
+    withDrawTotalElement.innerText=newWithDrawAmount;
+    withDrawField.value='';
+
+    const currentWithDraw=parseFloat(previousWithdrawTotal)+parseFloat(newWithDrawAmount);
+    withDrawTotalElement.innerText=currentWithDraw;
+
+    const balanceElement=document.getElementById('balance-total');
+    const priviousBalance=balanceElement.innerText;
+    const currentBalance=parseFloat(priviousBalance)-currentWithDraw;
+    balanceElement.innerText=currentBalance;
+
+    
+});
